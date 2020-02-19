e4a73e52b2f60bacc7f9ca9f5139995e04747b08
Destiny C <dbchamorro@yahoo.com>
2020-02-19 01:14:57 -0500

    Finished code
    Finished attempt at adding moving water, fire, wind

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -1,9 +1,23 @@
 extends Node2D
 
 onready var Camera = $Camera
+export add_trauma = 0
 
-func _ready():
-	pass
+
+func process(delta):
+	if Input.is_action_pressed("left"):
+		add_trauma += 0.2
+	if Input.is_action_pressed("right"):
+		add_trauma += 0.2
+	if Input.is_action_pressed("down"):
+		add_trauma +=0.2
+	if Input.is_action_pressed("up"):
+		add_trauma +=0.2
 
 func _physics_process(delta):
 	pass
+
+func add_trauma(amount):
+	trauma = min(trauma + amount, 1.0)
+
+#get_viewport().get_camera().add_trauma(0.2)
